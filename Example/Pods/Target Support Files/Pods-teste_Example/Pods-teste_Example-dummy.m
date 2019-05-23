ede6851e24ef01057d86c23e2061af08b4abfb97
gabrielfreitas <gabrielfreitascardoso@gmail.com>
2019-05-23 18:20:40 -0300

    teste

---
+++ Example/Pods/Target Support Files/Pods-teste_Example/Pods-teste_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_teste_Example : NSObject
+@end
+@implementation PodsDummy_Pods_teste_Example
+@end
---
+++ Example/Pods/Target Support Files/teste/teste-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_teste : NSObject
+@end
+@implementation PodsDummy_teste
+@end
